3eaef4e237dfb71df805bee098e7a011d769126f
iddrums <48152012+iddrums@users.noreply.github.com>
2021-06-15 09:16:20 +0100

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <article>
+        <h1>{!! $post->title !!}</h1>
+
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}
+
+        </p>
+
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+
+    <a href="/">Go Back</a>
+</x-layout>
