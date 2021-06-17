32a006519ecf9cebfcf60d38d6e5a32730b544a7
iddrums <48152012+iddrums@users.noreply.github.com>
2021-06-17 10:31:29 +0100

    first commit

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,13 @@
+@props(['trigger'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+    {{-- Trigger --}}
+    <div @click="show = ! show">
+        {{ $trigger }}
+    </div>
+
+    {{-- Links --}}
+    <div x-show="show" class="py-1 position-absolute bg-gray-100 mt-2 rounded-xl w-full z-50">
+       {{ $slot }}
+    </div>
+</div>
