a65df2a6a1fb3ef20b6141ec4bc66efa454609b6
iddrums <48152012+iddrums@users.noreply.github.com>
2021-06-17 08:39:32 +0100

    first commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,51 @@
 <x-layout>
-    <article>
-        <h1>{!! $post->title !!}</h1>
+    <section class="px-6 py-8">
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <article
+                class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+                <div class="py-6 px-5 lg:flex">
+                    <div class="flex-1 lg:mr-8">
+                        <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+                    </div>
 
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}
-         </p>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
+                    <div class="flex-1 flex flex-col justify-between">
+                        <header class="mt-8 lg:mt-0">
+                            <div class="space-x-2">
+                                <x-category-button :category="$post->category" />
+                            </div>
 
-    <a href="/">Go Back</a>
+                            <div class="mt-4">
+                                <h1 class="text-3xl">
+                                    {{ $post->title }}
+                                </h1>
+
+                                <span class="mt-2 block text-gray-400 text-xs">
+                                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                                    </span>
+                            </div>
+                        </header>
+
+                        <div class="text-sm mt-2">
+                            {{ $post->body }}
+                        </div>
+
+                        <footer class="flex justify-between items-center mt-8">
+                            <div class="flex items-center text-sm">
+                                <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                                <div class="ml-3">
+                                <h5 class="font-bold">{{ $post->author->name }}</h5>
+                                </div>
+                            </div>
+
+                            <div class="hidden lg:block">
+                                <a href="/"
+                                   class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
+                                >Read More</a>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
+        </main>
+    </section>
 </x-layout>
