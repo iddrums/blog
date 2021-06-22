df32fe4d3cc44908eb1ef5c77fac0e1c9b626ad6
iddrums <48152012+iddrums@users.noreply.github.com>
2021-06-22 16:30:27 +0100

    first coomit

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,14 +20,15 @@ class SessionsController extends Controller
           'password' => 'required'
         ]);
 
-        if (auth()->attempt($attributes)) {
-            session()->regenerate();
-            return redirect('/')->with('success', 'Welcome Back!!');
+        if (! auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Email Could not be verified'
+            ]);
         }
 
-        throw ValidationException::withMessages([
-            'email' => 'Email Could not be verified'
-        ]);
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!!');
     }
 
 
