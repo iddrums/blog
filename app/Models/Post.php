956b5fc89bfaae9b9e0ead344306291c3ddd1712
iddrums <48152012+iddrums@users.noreply.github.com>
2021-06-23 18:26:53 +0100

    first commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,13 +31,18 @@ class Post extends Model
                 $query->where('username', $author)));
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -46,6 +46,12 @@
                         </footer>
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                   @foreach ($post->comments as $comment)
+                     <x-post-comment :comment="$comment" />
+                   @endforeach
+                </section>
             </article>
         </main>
     </section>
