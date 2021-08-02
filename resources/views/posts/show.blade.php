78612b47356c1c729e014f169c2b2aa6d8d4744e
iddrums <48152012+iddrums@users.noreply.github.com>
2021-08-02 13:08:52 +0100

    episode 57

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,7 +20,8 @@
                                 </h1>
 
                                 <span class="mt-2 block text-gray-400 text-xs">
-                                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                                    Published
+                                <time>{{ $post->created_at->diffForHumans() }}</time>
                                 </span>
                             </div>
                         </header>
@@ -34,57 +35,26 @@
                                 <img src="/images/lary-avatar.svg" alt="Lary avatar">
                             <div class="ml-3">
                                    <h5 class="font-bold">
-                                       <a href="/?authors={{ $post->author->username }}">{{ $post->author->name }}</a></h5>
+                                       <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a></h5>
                                 </div>
                             </div>
 
                             <div class="hidden lg:block">
                                 <a href="/"
-                                class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                                >Read More</a>
+                                    class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read More
+                                </a>
                             </div>
                         </footer>
                     </div>
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                   @auth
-                    <x-panel>
-                        <form method="POST" action="/posts/{{ $post->slug }}/comments">
-                        @csrf
-
-                        <header class="flex items-center">
-                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="60" class="rounded-full">
-
-                            <h2 class="ml-4">Want to participate</h2>
-                        </header>
-
-
-                        <div class="mt-6">
-                            <textarea
-                                 name="body"
-                                 class="w-full text-sm focus:outline-none focus:ring"
-                                 rows="5"
-                                 placeholder="Quick think of something to say"></textarea>
-                        </div>
-
-                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                            <button type="submit"
-                                  class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
-                                Post</button>
-                        </div>
-                        </form>
-                   </x-panel>
-                @else
-                    <p class="font-semibold">
-                        <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Log in to leave a comment</a>
-                    </p>
+                    @include ('posts._add-comment-form')
 
                    @foreach ($post->comments as $comment)
-                     <x-post-comment :comment="$comment" />
+                       <x-post-comment :comment="$comment" />
                    @endforeach
                 </section>
-                @endauth
             </article>
         </main>
     </section>
