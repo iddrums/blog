46cbba3022984618c3025d82bb2909b2992c6bdd
iddrums <48152012+iddrums@users.noreply.github.com>
2021-08-05 19:06:54 +0100

    episode 68-69

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Response;
 
 class PostController extends Controller
 {
     public function index()
     {
-           return view('posts.index', [
-            'posts' => Post::latest()->filter(
-                          request(['search', 'category', 'author'])
-                     )->paginate(18)->withQueryString()
-           ]);
+        return view('posts.index', [
+        'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(18)->withQueryString()
+        ]);
     }
 
     public function show(Post $post)
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Services\Newsletter;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +29,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'idrumz';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()->can('admin');
+        });
     }
 }
